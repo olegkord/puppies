df7aa43f5a2386d0ba2d691c817e8aa9c4504e5a
Oleg <oleg.kord@gmail.com>
2015-12-17 22:24:17 -0500

    added index view and filled controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,53 @@
 class PostsController < ApplicationController
 
+  def index
+    @post = Post.all
 
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    post = Post.create(post_params)
+    if post.save
+      redirect_to '/posts'
+    else
+      redirect_to '/posts/new'
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    post = Post.find(params[:id])
+    if post.update(post_params)
+      redirect_to '/posts'
+    else
+      redirect_to '/posts/edit'
+    end
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_to '/posts'
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(
+      :title,
+      :body
+      :img_url
+    )
+  end
 end
